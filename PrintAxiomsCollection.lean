c0fa3b5bff7c4e8a9a575e86067abc0e415e6912
banri <banri.yanahama@gmail.com>
2025-12-04 23:30:23 +0900

    add collection of print axioms

---
+++ PrintAxiomsCollection.lean
@@ -0,0 +1,39 @@
+import Mathlib
+
+#print axioms Set
+#print axioms Nat
+#print axioms Int
+#print axioms Rat
+#print axioms Monoid
+#print axioms Group
+#print axioms Ring
+#print axioms Vector
+#print axioms MeasurableSpace
+#print axioms TopologicalSpace
+#print axioms Functor
+#print axioms T1Space
+#print axioms Lattice
+#print axioms HeytingAlgebra
+#print axioms BooleanAlgebra
+#print axioms List
+-- #print axioms Category
+-- #print axioms NaturalTransformation
+
+#print axioms Field
+#print axioms FermatLastTheorem
+
+#print axioms UniformSpace
+#print axioms T0Space
+#print axioms CompactSpace
+#print axioms Representation
+
+#print axioms Real
+#print axioms Complex
+#print axioms MetricSpace
+#print axioms NormedSpace
+#print axioms HilbertSpace
+#print axioms T2Space
+#print axioms TopologicalLattice
+#print axioms BoundarylessManifold
+#print axioms CategoryTheory.yonedaLemma
+#print axioms LieGroup
