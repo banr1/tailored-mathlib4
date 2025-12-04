d3946b907ac61ff713345def453c7b6959fe94ea
banri <banri.yanahama@gmail.com>
2025-12-05 02:51:21 +0900

    update axioms collection

--- PrintAxiomsCollection.lean
+++ PrintAxiomsCollection.lean
@@ -1,5 +1,6 @@
 import Mathlib
 
+-- concepts not depending on any axioms
 #print axioms Set
 #print axioms Nat
 #print axioms Int
@@ -16,17 +17,53 @@ import Mathlib
 #print axioms HeytingAlgebra
 #print axioms BooleanAlgebra
 #print axioms List
--- #print axioms Category
--- #print axioms NaturalTransformation
+#print axioms Empty
+#print axioms Option
+#print axioms Monad
+#print axioms CategoryTheory.Category
+#print axioms CategoryTheory.SmallCategory
+#print axioms CategoryTheory.LargeCategory
+#print axioms CategoryTheory.NatTrans
+#print axioms Preorder
+#print axioms PartialOrder
+#print axioms Semiring
+#print axioms Algebra
+#print axioms Module
+#print axioms Matrix
+#print axioms LinearMap
+#print axioms Submodule
+#print axioms Ideal
+#print axioms SimpleGraph
+#print axioms Locale
+#print axioms CategoryTheory.ObjectProperty
+#print axioms CategoryTheory.MorphismProperty
+#print axioms CommutatorRing
+#print axioms Filter
+#print axioms ContinuousMap
+#print axioms CategoryTheory.MarkovCategory
+#print axioms CategoryTheory.MonoidalCategory
+#print axioms Decidable
 
+
+-- concepts depending on propext
 #print axioms Field
 #print axioms FermatLastTheorem
 
+
+-- concepts depending on Quot.sound
+#print axioms Quotient.sound
+
+
+-- concepts depending on propext, Quot.sound
 #print axioms UniformSpace
 #print axioms T0Space
 #print axioms CompactSpace
 #print axioms Representation
+#print axioms Finset
+#print axioms Cauchy
 
+
+-- concepts depending on propext, Quot.sound, Classical.choice
 #print axioms Real
 #print axioms Complex
 #print axioms MetricSpace
@@ -37,3 +74,10 @@ import Mathlib
 #print axioms BoundarylessManifold
 #print axioms CategoryTheory.yonedaLemma
 #print axioms LieGroup
+#print axioms CategoryTheory.Adjunction
+#print axioms Sylow
+#print axioms CategoryTheory.CartesianClosed
+#print axioms CategoryTheory.MonoidalClosed
+#print axioms Homotopy
+#print axioms FundamentalGroup
+#print axioms FunctionField
